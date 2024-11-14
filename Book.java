889735c10838a9b64ee113e2d208af78a9df2f34
fcontre9 <fcontre9@asu.edu>
2024-11-13 23:00:28 -0700

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,5 +1,89 @@
 package application;
 
-public class Book {
+import java.io.Serializable;
 
+public class Book implements Serializable {
+	public int code;
+	public String title;
+	public String author;
+	public String category;
+	public String condition;
+	public double price;
+	public int chosen_number;
+
+		Book(){
+			
+		}
+		
+		Book(int code, String title, String author, String category, 
+				String condition, float price, int num){
+			this.code = code;
+			this.title = title;
+			this.author = author;
+			this.category = category;
+			this.condition = condition;
+			this.price = price;
+			this.chosen_number = num;
+			
+		}
+		public int getCode() {
+			return code;
+		}
+
+		public void setCode(int code) {
+			this.code = code;
+		}
+
+		public String getTitle() {
+			return title;
+		}
+
+		public void setTitle(String title) {
+			this.title = title;
+		}
+
+		public String getAuthor() {
+			return author;
+		}
+
+		public void setAuthor(String author) {
+			this.author = author;
+		}
+
+		public String getCategory() {
+			return category;
+		}
+
+		public void setCategory(String category) {
+			this.category = category;
+		}
+
+		public String getCondition() {
+			return condition;
+		}
+
+		public void setCondition(String condition) {
+			this.condition = condition;
+		}
+
+		public double getPrice() {
+			return price;
+		}
+
+		public void setPrice(double newPrice) {
+			this.price = newPrice;
+		}
+
+		public int getChosen_number() {
+			return chosen_number;
+		}
+
+		public void setChosen_number(int chosen_number) {
+			this.chosen_number = chosen_number;
+		}
+		public String toString() {
+		        return (title + " by " + author + " Price: $"+price+" Condition: "+condition);
+		    }
+
+		
 }
