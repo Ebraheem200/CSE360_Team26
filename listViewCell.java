4c1ead6a1572bafa8f9e7e565a0c789ba9380f4e
Ebraheem200 <111935532+Ebraheem200@users.noreply.github.com>
2024-11-17 10:22:36 -0700

    in progress

--- listViewCell.java
+++ listViewCell.java
@@ -1,5 +1,39 @@
 package application;
 
-public class listViewCell {
+import javafx.scene.control.ListCell;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+import javafx.scene.image.ImageView;
 
+public class ListViewCell extends ListCell<Book> {
+    private HBox content;
+    private Text name;
+    private Text author;
+    private Text category;
+    private ImageView thumbnail;
+
+    public ListViewCell() {
+        super();
+        name = new Text();
+        author = new Text();
+        category = new Text();
+        thumbnail = new ImageView();
+        content = new HBox(thumbnail, name, author, category);
+        content.setSpacing(10); // Adjust spacing between elements
+    }
+
+    @Override
+    protected void updateItem(Book book, boolean empty) {
+        super.updateItem(book, empty);
+        if (empty || book == null) {
+            setText(null);
+            setGraphic(null);
+        } else {
+            name.setText("Name: " + book.getName());
+            author.setText("Author: " + book.getAuthor());
+            category.setText("Category: " + book.getCategory());
+            thumbnail.setImage(book.getThumbnail());
+            setGraphic(content);
+        }
+    }
 }
