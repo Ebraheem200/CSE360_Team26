24788d9af539a40a2e8b8d442de97cea64e6d79d
saud526 <saudzaem@gmail.com>
2024-11-23 01:00:26 -0700

    listViewCell.java

--- listViewCell.java
+++ listViewCell.java
@@ -1,5 +1,34 @@
 package application;
+import javafx.scene.control.ListCell;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 
-public class listViewCell {
+public class listViewCell extends ListCell<Book> {
+protected void updateItem(Book book,  boolean empty) {
+    super.updateItem(book, empty);
+  if (empty || book == null){
+         setText(null);
+         setGraphic(null);
+  } else { 
+    HBox hbox = new HBox();
+    Text titleText = new Text(book.getTitle());
+    Text authorText = new Text(" by " + book.getAuthor());   
 
+    ImageView coverImageView = null;
+    if (book.getCoverImage() != null  && !book.getCoverImage().isEmpty(){
+      Image CoverImage = new Image("file:" + book.getCoverImage()); 
+      coverImageView.setFitHeight(50);
+      coverImageView.setFitWidth(50);
+    }
+    if (coverImageView != null) {
+      hbox.getChildren().add(coverImageView);
+      hbox.getChlidren().add(authorText);
+
+      setGraphic(hbox);
+    }
+  }
 }
+    
+    
